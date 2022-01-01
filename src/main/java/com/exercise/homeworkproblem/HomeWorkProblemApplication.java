e8ee7a9dbe428d24f8720b3c5927be4ceab8c7d7
Matias Solana Mendez <matiasmendez@ibm.com>
2022-01-01 13:54:07 -0300

    first commit

---
+++ src/main/java/com/exercise/homeworkproblem/HomeWorkProblemApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.homeworkproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkProblemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exercise/homeworkproblem/HomeWorkProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.homeworkproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
